b496b38271dd84db86b3c162a47da5853b9061c7
KennedyAlvess <kenedy199717@gmail.com>
2022-03-18 20:11:41 -0300

    Comentario sobre complexidade adicionado ao fim do código

--- Roteiro do Laboratório 01/MediaInterativaRecursiva.c
+++ Roteiro do Laboratório 01/MediaInterativaRecursiva.c
@@ -1,30 +1,25 @@
 #include <stdlib.h >
 #include <stdio.h >
 #include <time.h >
-#include <math.h>
 
 //Criação da função para calcular a media de forma interativa
-float media_interativa(float *res, int n){
+float media_interativa(float vetor[], int n){
 
-    if(n == 0){ //condição caso não tenha nenhum numero no vetor - também utilizado na função recursiva
-        return 0;
+    float soma_interativa = 0;
+    for( int i = 0; i < n ; i++){
+        soma_interativa += vetor[i];
     }
-    else{
-        float soma_interativa = 0;
-        for( int i = 0; i < n ; i++){
-            soma_interativa += res[i];
-        }
-        float mediaInterativa = (1/(float)n)*soma_interativa;
-        return mediaInterativa;
-    }
+    return soma_interativa / n;
+
 }
 
 //Criação da função  para calcular a media de forma recursiva
-float media_recursiva(float *res, int n){
-    if (n == 0) {
-        return 0;}
+float media_recursiva(float vetor[], int n){
+    if (n == 1) {
+        return vetor[0];}
     else{
-        return (res[n - 1] + (n-1)*media_recursiva(res, n - 1)) / n;}//Parte recursiva onde chama a função dentro da propria função
+        return (vetor[n - 1] + (n-1)*media_recursiva(vetor, n - 1)) / n;
+        }
 }
 
 
@@ -40,20 +35,14 @@ float* gerador ( size_t n) {
  }
 
  int main () {
-    printf ("Sequencia aleatoria \n");
-    size_t n = 100000;
-    float *res = gerador (n);
-    
-    for ( size_t i = 0; i < n ; i ++) {
-        printf ("res [%lu]=%f\n", i , res [i]);
-    }
 
-    float MI = media_interativa(res,n);
-    printf("Media Interativa dos numeros: %f\n",MI);
-    float MR = media_recursiva(res,n);
-    printf("Media Recursiva dos numeros: %f\n",MR);
+    size_t n = 10000;
+    float *numeros = gerador (n);
 
-/*Discutindo com o meu colega de grupo acabamos por perceber algo interessante na complexidade das duas
-funções, a função recursiva chegou a ser mais complexa do que a interativa, fazendo com que nem chegasse 
-a ser executada corretamente em alguns casos*/
+    float MI = media_interativa(numeros,n);
+    printf("\nMedia Interativa dos numeros: %f\n",MI);
+    float MR = media_recursiva(numeros,n);
+    printf("\nMedia Recursiva dos numeros: %f\n",MR);
+    free(numeros);
+/*Ao ponto de vista do grupo, chegamos a conclusão que a complexidade de ambas funções são O(n)*/
  }
